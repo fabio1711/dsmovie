69b715fb7e978e3c6a630b5fb076ee79a97262cb
Fabio Fernandes <fabio-fer@hotmail.com>
2022-05-14 19:19:17 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,28 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
+import { BASE_URL } from "utils/requests";
 import MovieCard from "../../components/MovieCard";
 import Pagination from "../../components/Pagination";
 
 function Listing() {
+
+  const [pageNumber,setPageNumber] = useState(0);
+
+  useEffect(() => {
+    axios.get(${BASE_URL} /movies?size=128page=1)
+    .then(response=>{
+      const data = response.data as MoviePage;
+     console.log(data);
+      setPageNumber(data.number);
+    });
+
+  },[])
+
+  //forma errada
+ 
+
+
   return (
     <>
       <Pagination />
